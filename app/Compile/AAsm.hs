adb2f777d7a513000f0f77b368e1f3206423b910
Arne Keller <arne.keller@posteo.de>
2025-05-06 16:46:23 +0200

    Addition

--- app/Compile/AAsm.hs
+++ app/Compile/AAsm.hs
@@ -4,16 +4,18 @@ module Compile.AAsm
   ( codeGen
   ) where
 
-import           Compile.AST (AST(..), Expr(..), Stmt(..), showAsgnOp)
+import           Compile.AST (AST(..), Expr(..), Stmt(..), showAsgnOp, Op (..))
 
 import           Control.Monad.State
 import qualified Data.Map as Map
 
 import TH (litFile)
+import Error (parserFail)
 
 type Register = Integer
 
 type VarName = String
+type TempVarIndex = Integer
 
 type AAsmAlloc = Map.Map VarName Register
 
@@ -22,20 +24,63 @@ type CodeGen a = State CodeGenState a
 data CodeGenState = CodeGenState
   { regMap :: AAsmAlloc
   , nextReg :: Register
+  , nextTempVar :: Integer
   , code :: [String]
   }
 
 assemblyTemplate :: String
 assemblyTemplate = [litFile|app/Compile/assembly_template.s|]
 
+simplifyStmts :: [Stmt] -> CodeGen [Stmt]
+simplifyStmts [ ] = return [ ]
+simplifyStmts ( x : xs ) = do
+  newHead <- case x of
+    (Decl _ _) -> return [ x ]
+    (Init _ (Ident _ _) _) -> return [ x ]
+    (Init _ (IntExpr _ _) _) -> return [ x ]
+    _ -> return [ x ]
+
+  simplifyWhatExtra <- if length newHead == 1 then return [ ] else return newHead
+  simplifyDone <- if length newHead == 1 then return newHead else return []
+  rest <- simplifyStmts $ simplifyWhatExtra ++ xs
+  return $ simplifyDone ++ rest
+
+
 codeGen :: AST -> [String]
 codeGen (Block stmts _) = lines assemblyTemplate ++ code (execState (genBlock stmts) initialState)
   where
-    initialState = CodeGenState Map.empty 0 []
+    initialState = CodeGenState Map.empty 0 0 []
+
+--codeGen :: AST -> [String]
+--codeGen ast = code (execState (codeGen2 ast) initialState)
+--  where initialState = CodeGenState Map.empty 0 0 []
+--
+--codeGen2 :: AST -> CodeGen [String]
+--codeGen2 (Block stmts _) = do
+--  -- stmtsSimple <- simplifyStmts stmts
+--  genBlock  stmts --stmtsSimple
+--  curr <- get
+--  return $ lines assemblyTemplate ++ code curr
 
 regName :: Register -> String
 regName 0 = "eax"
-regName _ = fail "out of registers :("
+regName 1 = "ebx"
+regName 2 = "ecx"
+regName 3 = "edx"
+regName 4 = "esi"
+regName 5 = "edi"
+regName 6 = "ebp"
+regName 7 = "esp"
+regName 8 = "r8d"
+regName 9 = "r9d"
+regName 10 = "r10d"
+regName 11 = "r11d"
+regName 12 = "r12d"
+regName 13 = "r13d"
+regName 14 = "r14d"
+regName 15 = "r15d"
+-- TODO: use xmm registers :)
+regName _ = error "out of registers :("
 
 freshReg :: CodeGen Register
 freshReg = do
@@ -43,6 +88,14 @@ freshReg = do
   let r = nextReg curr
   put curr {nextReg = r + 1}
   return r
+
+freshTempVar :: CodeGen VarName
+freshTempVar = do
+  curr <- get
+  let r = nextTempVar curr
+  put curr {nextTempVar = r + 1}
+  return ("tempCompilerInternal" ++ show r)
+
 
 assignVar :: VarName -> Register -> CodeGen ()
 assignVar name r = do
@@ -71,7 +124,9 @@ genStmt (Init name e _) = do
 genStmt (Asgn name op e _) = do
   rhs <- genExpr e
   lhs <- lookupVar name
-  emit $ regName lhs ++ showAsgnOp op ++ regName rhs
+  case op of
+    Nothing -> emit $ "mov " ++ regName lhs ++ ", " ++ regName rhs
+    _ -> error "uh oh, I don't know that operator :("
 genStmt (Ret e _) = do
   r <- genExpr e
   emit $ "mov " ++ regName 0 ++ ", " ++ regName r
@@ -86,11 +141,19 @@ genExpr (Ident name _) = lookupVar name
 genExpr (UnExpr op e) = do
   r1 <- genExpr e
   r <- freshReg
-  emit $ regName r ++ " = " ++ show op ++ " " ++ regName r1
+  instrName <- case op of
+    Neg -> return "neg"
+    _ -> error "illegal unary operation"
+  emit $ "mov " ++ regName r ++ ", " ++ regName r1
+  emit $ instrName ++ " " ++ regName r
   return r
 genExpr (BinExpr op e1 e2) = do
   r1 <- genExpr e1
   r2 <- genExpr e2
   r <- freshReg
-  emit $ regName r ++ " = " ++ regName r1 ++ " " ++ show op ++ " " ++ regName r2
+  instrName <- case op of 
+    Add -> return "add"
+    _ -> error "unknown binary operation"
+  emit $ "mov " ++ regName r ++ ", " ++ regName r1
+  emit $ instrName ++ " " ++ regName r ++ ", " ++ regName r2
   return r
