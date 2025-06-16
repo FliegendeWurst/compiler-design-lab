0000013b6e6ed0760066d39f7d5b898868acd589
Arne Keller <arne.keller@posteo.de>
2025-06-16 09:24:18 +0200

    Update test generator for L2
    Recommend to run using:
    
    while true; do python3 generate_alphabet_soup.py > test.c && gcc -Wno-error=implicit-function-declaration test.c -o test 2>/dev/null && echo "Running test..."; timeout 0.0001s ./test | rg -m1 done; [[ $? == 0 ]] && break; done

--- generate_alphabet_soup.py
+++ generate_alphabet_soup.py
@@ -1,19 +1,54 @@
 from random import randint,choice
 from string import ascii_lowercase
 
-target = ascii_lowercase[:5]
+num_vars = 3
+num_loops = 7
+loop_vars = 1
+
+def op():
+    return choice("+-*")
+
+target = ascii_lowercase[:num_vars]
+
+def var():
+    return choice(target)
+
+def comp_op():
+    return choice([">=", "<=", ">", "<"])
+
+def reassign_all():
+    for x in target:
+        print(" ", x, "=", end=" ")
+        for y in target[:-1]:
+            print(y, op(), end=" ")
+        print(target[-1], end=";\n")
 
 print("int main() {")
+for xx in target:
+    val = randint(10,100)
+    print("  int", xx, "=", val, end=";\n");
+    globals()[xx] = val
 
-for x in target:
-    print("  int", x, "=", randint(10,100), end=";\n");
+for l in range(num_loops):
+    print("while(", end="")
+    while True:
+        cond = ""
+        for lv in range(loop_vars):
+            if lv > 0:
+                cond += op()
+            cond += var()
+        cond += comp_op()
+        cond += str(randint(-2**10, 2**10))
+        if eval(cond.replace("/", "//")):
+            break
+    print(cond, end="")
+    print(') {printf("%s\\n");' % l)
 
-for x in target:
-    print(" ", x, "=", end=" ")
-    for y in target[:-1]:
-        print(y, choice("+-/*%"), end=" ")
-    print(target[-1], end=";\n")
+# reassign_all()
 
-
+for _l in range(num_loops):
+    reassign_all()
+    print("}")
+print('printf("done\\n");')
 print("  return", target[-1], ";")
 print("}")
